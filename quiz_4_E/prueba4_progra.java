cc1ddce50d00d3ce66414a6cefac63e982d1e112
carranzaelberth505-del <carranzaelberth505@gmail.com>
2025-09-02 17:30:47 -0600

    cosa rara

---
+++ quiz_4_E/prueba4_progra.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logic;
+
+/**
+ *
+ * @author Lenovo X1
+ */
+public class prueba4_progra {
+
+    public void prueba4_progra() {
+    System.out.println ("espero estar haciendo esto bien");
+    }
+    
+
+}
